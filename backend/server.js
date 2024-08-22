ea3effddffaed7e0c621190815e23ac6937ff3fe
hdjwdn <172260858+hdjwdn@users.noreply.github.com>
2024-08-22 23:44:36 +0800

    bug

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // 启用 CORS
 app.use(cors());
+
+// 使用 body-parser 处理 JSON 数据
+app.use(bodyParser.json());
 
 // 存储留言的数组
 let messages = [];
@@ -14,12 +18,12 @@ let messages = [];
 app.use(express.static(path.join(__dirname, '../')));
 
 // 处理 GET 请求，返回所有留言
-app.get('/api/messages', (req, res) => {
+app.get('/messages', (req, res) => {
     res.json(messages);
 });
 
 // 处理 POST 请求，接收新的留言
-app.post('/api/messages', (req, res) => {
+app.post('/messages', (req, res) => {
     const { name, message } = req.body;
     if (name && message) {
         const newMessage = { name, message, date: new Date() };
@@ -30,8 +34,15 @@ app.post('/api/messages', (req, res) => {
     }
 });
 
-app.get('*', (req, res) => {
+// 处理根路径请求，返回首页
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+// 处理所有其他请求，返回 404 错误
+app.use((req, res) => {
+    res.status(404).send('404 Not Found');
+});
+
 
 module.exports = app;
